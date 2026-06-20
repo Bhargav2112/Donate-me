80c562575d6e7fc64f4c607d968a1c2ac8587432
Bhargav2112 <bsparmar1221@gmail.com>
2026-06-20 17:12:51 +0530

    add scroller in login pages and remove registration options

--- donate-me/src/pages/Login.jsx
+++ donate-me/src/pages/Login.jsx
@@ -37,14 +37,6 @@ export default function Login() {
       icon={LogIn}
       title="Welcome back"
       subtitle="Log in to your account"
-      footer={
-        <>
-          Don't have an account?{" "}
-          <Link to="/register" className="text-primary font-medium hover:underline">
-            Create one
-          </Link>
-        </>
-      }
     >
       <Button
         variant="outline"
